8d50afb0102e369c84749cd47000e1045260c715
lerakirpu <lerakirpu@mail.ru>
2025-10-24 23:25:26 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,5 @@
 
 int main(int, char**){
     std::cout << "Hello, World! Version " << VERSION_PROJECT << std::endl;
-    
-    double a, b;
-    std::cout << "Введите первое число: ";
-    std::cin >> a;
-    std::cout << "Введите второе число: ";
-    std::cin >> b;
-    
-    std::cout << "Сумма: " << a + b << std::endl;
-    std::cout << "Разность: " << a - b << std::endl;
-    
     return 0;
 }
