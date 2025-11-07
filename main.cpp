bf30033fb7135e3cea78f46777346123c6a2d33e
lerakirpu <lerakirpu@mail.ru>
2025-11-07 19:07:31 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 
-#ifndef VERSION_PROJECT
-#define VERSION_PROJECT "1.0.2"
-#endif
-
 int main(int, char**){
-    std::cout << "Hello, World! Version " << VERSION_PROJECT << std::endl; 
+    std::cout << "Hello, World! Version " << VERSION_PROJECT << std::endl;
     return 0;
 }
